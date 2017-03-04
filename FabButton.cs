24769fabc6b22e1c2e0346e5740f75cae621a3b5
Matt Soucoup <msoucoup@codemilltech.com>
2017-03-04 14:08:18 -0600

    Initial commit

---
+++ FabButton.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UIKit;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+
+namespace FabLib
+{
+	public class FabButton : UIView
+	{
+		CAShapeLayer circleLayer;
+		CAShapeLayer tintLayer;
+
+		CAShapeLayer downLayer;
+		CAShapeLayer crossLayer;
+
+		public event EventHandler<EventArgs> ButtonPressed;
+
+
+		public FabButton() : base(new CGRect(0, 0, 40, 40))
+		{
+			Initialize();
+		}
+
+		public FabButton(CGRect rect) : base(rect)
+		{
+			Initialize();
+		}
+
+		void Initialize()
+		{
+			BackgroundColor = UIColor.Clear;
+			UserInteractionEnabled = true;
+		}
+
+		public override void Draw(CGRect rect)
+		{
+			// This will always end up 40x40 with the original x,y
+			var fabRect = new CGRect(rect.Location, new CGSize(40, 40));
+			fabRect = rect;
+
+			base.Draw(fabRect);
+
+			circleLayer = new CAShapeLayer();
+			var bp = UIBezierPath.FromOval(fabRect);
+			circleLayer.Path = bp.CGPath;
+			circleLayer.FillColor = UIColor.FromRGB(230, 248, 37).CGColor;
+			Layer.AddSublayer(circleLayer);
+
+			tintLayer = new CAShapeLayer();
+			tintLayer.Path = bp.CGPath;
+			tintLayer.FillColor = UIColor.FromRGB(168, 181, 27).CGColor;
+
+			// Calculate the X and Y for the cross rectangles
+			var xMidPointLow = fabRect.GetMidX() - 2;
+			var xMidPointHigh = fabRect.GetMidX() + 2;
+			var xPointLow = fabRect.GetMinX() + 8;
+			var xPointHigh = fabRect.GetMaxX() - 8;
+
+			var yMidPointLow = fabRect.GetMidY() - 2;
+			var yMidPointHigh = fabRect.GetMidY() + 2;
+			var yPointLow = fabRect.GetMinY() + 8;
+			var yPointHigh = fabRect.GetMaxY() - 8;
+
+			downLayer = new CAShapeLayer();
+			var downRect = new CGRect(xMidPointLow, yPointLow, xMidPointHigh - xMidPointLow, yPointHigh - yPointLow);
+			var downBp = UIBezierPath.FromRoundedRect(downRect, 2);
+			downLayer.Path = downBp.CGPath;
+			downLayer.FillColor = UIColor.FromRGB(232, 80, 80).CGColor;
+			Layer.AddSublayer(downLayer);
+
+			crossLayer = new CAShapeLayer();
+			var crossRect = new CGRect(xPointLow, yMidPointLow, xPointHigh - xPointLow, yMidPointHigh - yMidPointLow);
+			var crossBp = UIBezierPath.FromRoundedRect(crossRect, 2);
+			crossLayer.Path = crossBp.CGPath;
+			crossLayer.FillColor = UIColor.FromRGB(232, 80, 80).CGColor;
+			Layer.AddSublayer(crossLayer);
+		}
+
+		public override void TouchesBegan(NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan(touches, evt);
+			Layer.InsertSublayerBelow(tintLayer, downLayer);
+		}
+
+		public override void TouchesEnded(NSSet touches, UIEvent evt)
+		{
+			base.TouchesEnded(touches, evt);
+
+			tintLayer.RemoveFromSuperLayer();
+
+			ButtonPressed?.Invoke(this, new EventArgs());
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pluralsight-ios-fab
-Example code for iOS floating action button from Moving Beyond the Basics with Xamarin.Forms course
+# Demo iOS FAB
+FAB as demo'd in fourth module of the [Moving Beyond the Basics with Xamarin.Forms](https://www.pluralsight.com/courses/xamarin-forms-moving-beyond-basics) course.
+
+## Implementation
+This is a very simple example, and only meant as a jumping off point. The circle and the "plus sign" on the button were implemented with CoreGraphics. 
+
+The drawing of the button occurs in the `Draw` function, which is an overridden function provided by the `UIView` class - which this button derives from.
+
+The `TouchesBegan` and `TouchesEnded` functions are overridden to provide a means for the button to report when the user is interacting with it. During the `TouchesBegan` a "tinting" layer is inserted into the view hierarchy - so provide something of a shadow - so the user can see it's being tapped.
+
+Finally, there is a very simple `EventHandler<EventArgs> ButtonPressed` which gets invoked during the `TouchesEnded` function. This is how the button notifies any subscribers that it has been interacted with. (The `EventArgs` is probably unnecessary in this case - since we're only saying it's been touched.)
+
+Hope this helps - and don't forget to rate the course 5 stars! :)
